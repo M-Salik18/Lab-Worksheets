dd1800aaaca810da233c9f308ad00473a1ca5b1e
M-Salik18 <salikrizwan2024@gmail.com>
2025-09-12 16:58:53 +0530

    Update Q5.java

--- src/LW_01/Q5.java
+++ src/LW_01/Q5.java
@@ -4,15 +4,15 @@ public class Q5 {
     public static void main(String[] args) {
         Circle outerCircle = new Circle(14);
         Circle innerCircle = new Circle(7);
-        
+
         double outerCircleArea = outerCircle.computeArea();
         double innerCircleArea = innerCircle.computeArea();
         double shadedArea = innerCircleArea - outerCircleArea;
-        
-        double outerCircleCircum = outerCircle.computeCircum();
-        double innerCircleCircum = innerCircle.computeCircum();
+
+        double outerCircleCircum = outerCircle.computeCircumference();
+        double innerCircleCircum = innerCircle.computeCircumference();
         double shadedCircum = innerCircleCircum - outerCircleCircum;
 
-       
+
     }
 }

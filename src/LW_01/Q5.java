55d3efac633d4050756365c54cd5f4e452b6261f
M-Salik18 <salikrizwan2024@gmail.com>
2025-09-12 16:32:17 +0530

    Update Q5.java

--- src/LW_01/Q5.java
+++ src/LW_01/Q5.java
@@ -2,9 +2,15 @@ package LW_01;
 
 public class Q5 {
     public static void main(String[] args) {
-       Circle c1 = new Circle();
-       c1.setRadius();
-       c1.ComputeArea();
-       c1.ComputeCircumference();
+       Circle outerCircle = new Circle(14);
+        Circle innerCircle = new Circle(7);
+        double outerCircleArea = outerCircle.computeArea();
+        double innerCircleArea = innerCircle.computeArea();
+        double shadedArea = innerCircleArea - outerCircleArea;
+        double outerCircleCircum = outerCircle.computeCircum();
+        double innerCircleCircum = innerCircle.computeCircum();
+        double shadedCircum = innerCircleCircum - outerCircleCircum;
+
+       
     }
 }

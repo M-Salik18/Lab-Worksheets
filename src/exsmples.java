c735d83ec1b90b33cb8423a36687e4aec549e92a
M-Salik18 <salikrizwan2024@gmail.com>
2025-11-25 21:32:36 +0530

    init commit

---
+++ src/exsmples.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+public class exsmples {
+    public static void main(String[] args) {
+        int month_days[] = new int[12];
+                float roundedValue = Math.round(3.6);
+                double ceilValue = Math.ceil(4.2);
+                double floorValue = Math.floor(5.8);
+                int absoluteValue = Math.abs(-10);
+                System.out.println("Rounded Value: " + roundedValue);
+                System.out.println("Ceiling Value: " + ceilValue);
+                System.out.println("Floor Value: " + floorValue);
+                System.out.println("Absolute Value: " + absoluteValue);
+
+
+        }
+}

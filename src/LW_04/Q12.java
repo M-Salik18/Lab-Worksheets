fa066f470a599de315cb361b33bd286960482c5d
M-Salik18 <salikrizwan2024@gmail.com>
2026-01-03 17:16:45 +0530

    init commit

--- src/LW_04/Q12.java
+++ src/LW_04/Q12.java
@@ -4,8 +4,23 @@ import java.util.Scanner;
 
 public class Q12 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String sentence = input.nextLine();
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String sentence = sc.nextLine();
+
+        System.out.println("Enter the word to be replaced:");
+        String oldWord = sc.nextLine();
+
+        System.out.println("Enter the replacement word:");
+        String newWord = sc.nextLine();
+
+        String updatedSentence = sentence.replace(oldWord, newWord);
+        System.out.println("\nUpdated Sentence:");
+        System.out.println(updatedSentence);
+
+        sc.close();
     }
 }
+
